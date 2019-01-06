8c6e68290feaefccf6ddc3e33b173f0b9b076779
abialas <abialas@universal-soft.com>
2019-01-06 22:15:39 +0100

    HelloWorld servlet (without JSP)

---
+++ helloworld-servlet/src/main/java/pl/sdaacademy/HelloWorldServlet.java
@@ -0,0 +1,30 @@
+package pl.sdaacademy;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+/**
+ * Created by adam.
+ */
+@WebServlet(value = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>" +
+                "<head><title>Hello World Servlet</title></head>" +
+                "<body>" +
+                "<p>Hello World! <br>Dzisiaj jest " + LocalDateTime.now() + "!</p>" +
+                "</body>" +
+                "</html>");
+
+    }
+}
