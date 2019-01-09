b35797bdd2dbf2bbadc69729470e87c420e350a9
abialas <abialas@universal-soft.com>
2019-01-09 22:06:44 +0100

    Download simple text file without default name, just with usage of URL

---
+++ download-servlet/src/main/java/pl/sdacademy/DownloadServlet.java
@@ -0,0 +1,36 @@
+package pl.sdacademy;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by adam.
+ */
+@WebServlet(name = "Download", value = "/Download.do")
+public class DownloadServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/octet-stream");
+
+        ServletContext servletContext = getServletContext();
+        InputStream inputStream = servletContext.getResourceAsStream("/welcome.txt");
+        ServletOutputStream responseOutputStream = response.getOutputStream();
+
+        byte[] buffer = new byte[8 * 1024];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            responseOutputStream.write(buffer, 0, bytesRead);
+        }
+        responseOutputStream.flush();
+        responseOutputStream.close();
+    }
+    
+}
